d9472294879729a9fae21ef5040720e6a7778508
brytebee <brytebee@gmail.com>
2021-12-01 14:13:27 +0100

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import Calculator from './components/calculator';
+import { Routes, Route } from 'react-router-dom';
 import Nav from './pages/Nav';
 import Footer from './pages/Footer';
 import './App.css';
 import Home from './pages/Home';
+import Calculator from './components/calculator';
 import Quote from './pages/Quote';
 
 const App = () => (
   <>
     <Nav />
-    <Home />
-    <Quote />
-    <Calculator />
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/quote" element={<Quote />} />
+      <Route path="/calculator" element={<Calculator />} />
+    </Routes>
     <Footer />
   </>
 );
