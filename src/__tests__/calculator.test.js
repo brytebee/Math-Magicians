4812049f3facbd258abc06485addbf9848621fcd
brytebee <brytebee@gmail.com>
2021-12-02 16:48:59 +0100

    Add Calculator component test/snapshot

--- src/__tests__/calculator.test.js
+++ src/__tests__/calculator.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { cleanup } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import Calculator from '../components/calculator';
 
 afterEach(cleanup);
 
-it('Renders calculator component correctly', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Calculator />, div);
+describe('Calcutor component', () => {
+  it('Renders calculator component correctly', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Calculator />, div);
+  });
+
+  it('Render Calculator component snapshot.', () => {
+    const tree = renderer.create(<Calculator />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
