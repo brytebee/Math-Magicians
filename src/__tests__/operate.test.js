0a78feffac6b1c2e474eee43af964a774884e77b
brytebee <brytebee@gmail.com>
2021-12-02 11:09:36 +0100

    Add another modulo test

--- src/__tests__/operate.test.js
+++ src/__tests__/operate.test.js
@@ -8,22 +8,29 @@ describe('Check if operators are working', () => {
   test('check addition', () => {
     expect(parseInt(operate(NumOne, NumTwo, '+'))).toBe(8);
   });
+
   //   Subtraction test
   test('check subtraction', () => {
     expect(parseInt(operate(NumOne, NumTwo, '-'))).toBe(4);
   });
-    //   Division test
-    test('check Division', () => {
-      expect(parseInt(operate(NumOne, NumTwo, '÷'))).toBe(3);
-    });
-    // Multiplication test
-    test('check Multiplication', () => {
-      expect(parseInt(operate(NumOne, NumTwo, 'x'))).toBe(12);
-    });
-    // Modulus test
-    test('check Modulus', () => {
-      expect(parseInt(operate(NumOne, NumTwo, '%'))).toBe(0);
-    });
+
+  //   Division test
+  test('check Division', () => {
+    expect(parseInt(operate(NumOne, NumTwo, '÷'))).toBe(3);
+  });
+
+  // Multiplication test
+  test('check Multiplication', () => {
+    expect(parseInt(operate(NumOne, NumTwo, 'x'))).toBe(12);
+  });
+
+  // Modulus test
+  test('check Modulus', () => {
+    expect(parseInt(operate(NumOne, NumTwo, '%'))).toBe(0);
+  });
+
+  //Modulus test 2
+  test('check modulus with numbers passed', () => {
+    expect(parseInt(operate(8, 3, '%'))).toBe(2);
+  });
 });
-
-
