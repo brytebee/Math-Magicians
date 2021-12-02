f0c6c35835aada6c17a9d3a29a3394ea4600bb56
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-12-02 17:07:26 +0200

    Add snapshot test for Footer page

--- src/__tests__/pages.test.js
+++ src/__tests__/pages.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Home from "../pages/Home";
 import Quote from "../pages/Quote";
+import Footer from '../pages/Footer';
+
 
 
 describe('Test that all components are rendering just fine.', () => {
@@ -16,6 +18,13 @@ describe('Test that all components are rendering just fine.', () => {
   it('Render the Quote page', () => {
     const tree = renderer
       .create(<Quote />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Render the Footer page', () => {
+    const tree = renderer
+      .create(<Footer />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
