1e3c7aabf8662e37cc460d456bbaa1beaf96c762
brytebee <brytebee@gmail.com>
2021-12-03 12:36:58 +0100

    Add quotes from "https://api.quotable.io/random" API service

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,10 +1,19 @@
+const baseUrl = 'https://api.quotable.io/random';
+
 const Quote = () => (
   <div className="content-container">
     <div className="content">
       <h1>Quote Page</h1>
-      <p>Our temporary Quote page</p>
+      <p>Sample Quote</p>
     </div>
   </div>
 );
 
+const quotes = async () => {
+  const req = await fetch(baseUrl);
+  const res = await req.json();
+  return res;
+};
+
 export default Quote;
+export { quotes };
